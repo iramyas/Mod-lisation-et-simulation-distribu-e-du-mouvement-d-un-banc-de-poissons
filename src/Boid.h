eae9631c67311ba6c87efbbce5f4ab177f043a2a
iram MADANI FOUATIH <iram.madanifouatih@uvsq.fr>
2025-11-14 11:37:16 +0100

    init

---
+++ src/Boid.h
@@ -0,0 +1,31 @@
+//definition de l'agent 
+
+#include <Vector2D.h>
+#include <vector>
+
+
+class Boid {
+public:
+    Vector2D position;
+    Vector2D velocity;
+    Vector2D accelération;
+
+    //parametres physiques
+    float mass;
+    float maxspeed;
+
+    //constructeurs
+    Boid();
+    //
+
+    //methodes de mise a jour
+
+
+    //les 3 regles de reynolds
+
+    //gestion de bords
+
+private:
+    //prendre en agruments un vecteur de pointeurs vers les objets boid pour trouver et renvoyer la liste des voisins
+
+};
---
+++ src/Vector2D.h
@@ -0,0 +1,20 @@
+#include <cmath>
+
+
+class Vector2D {
+public:
+    float x,y;
+
+    //constructeur
+
+
+    //operations arithmetiques + - / 
+
+    //operateurs d'affectation +=  
+
+    //methodes vectorielles  ^2 normalized 
+
+    //calculer l'angle
+
+
+};
