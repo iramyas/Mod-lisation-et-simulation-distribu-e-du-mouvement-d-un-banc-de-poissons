f0007d58bf8d9b6fd0aae6f623f3e2113af3985d
iram MADANI FOUATIH <iram.madanifouatih@uvsq.fr>
2025-11-20 12:44:07 +0100

    Premier test avec SFML

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,20 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, banc de poisson" << std::endl;
+    sf::VideoMode mode({800u, 600u});
+    sf::RenderWindow window(mode, "Hello, banc de poisson");
+
+    while (window.isOpen()) {
+        while (auto event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+        window.clear();
+        window.display();
+    }
+
+    std::cout << "Fenetre SFML FERME AVEC SUCCES.." << std::endl;
+    return 0;
 }
