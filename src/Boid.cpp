959868d9917e61f3ba77ac593a01b2778a79c828
iram MADANI FOUATIH <iram.madanifouatih@uvsq.fr>
2025-12-16 16:14:11 +0100

    suppresion de commentaires

--- src/Boid.cpp
+++ src/Boid.cpp
@@ -34,7 +34,7 @@ simulation::Boid::Boid(Vector2D pos, Vector2D vel)
 
 
 void simulation::Boid::update(float deltaTime){
-  velocity += acceleration * deltaTime;       //mettre a jour velocity avec aceleration
+  velocity += acceleration * deltaTime;       
 
   if (velocity.magnitude() > maxSpeed) {velocity = velocity.normalized() *maxSpeed;}    //limiter la vitesse a maxSpeed
 
@@ -71,18 +71,18 @@ std::vector<simulation::Boid*> simulation::Boid::getNeighbors(const std::vector<
 
 simulation::Vector2D simulation::Boid::separate(const std::vector<Boid*>& boids){
   Vector2D steering(0.0f, 0.0f);
-  int count=0;   //compte le nombre de voisins et sers pour faire la moyenne a la fin
-  for (Boid* other :boids){  //boucle sur chaque pointeur other dans le vecteur boids
+  int count=0;   
+  for (Boid* other :boids){  
     if (other == this) continue; 
-    Vector2D diff= position -other->position;         //calcule le vecteur diff allant de l'autre boid vers celui ci
-    float d= diff.magnitude();      //calcule la distance entre 2 boids 
-    if (d> 0.0f && d < perceptionRadius){  //distance strict positive . si  un boid est trop loin ca n'influence pas la separation
-      diff= diff / d ;   //il manque l'operateur division par scalaire dans Vector2D
+    Vector2D diff= position -other->position;         
+    float d= diff.magnitude();      
+    if (d> 0.0f && d < perceptionRadius){  
+      diff= diff / d ;   
       steering += diff;
       count++;
     }
   }
-  if (count > 0){steering = steering /static_cast<float>(count);}//moyenne 
+  if (count > 0){steering = steering /static_cast<float>(count);}
   else { return Vector2D(0.0f, 0.0f);}
   if (steering.magnitude() > 0.0f){
     steering = steering.normalized() *maxSpeed;
@@ -113,12 +113,12 @@ simulation::Vector2D simulation::Boid::align(const std::vector<Boid*>& boids){
     if (steering.magnitude()>0.0f) {
       steering= steering.normalized()*maxSpeed;
     }
-    steering -= velocity;     // le boid doit s'ajuster a la meeme vitesse que ses voisins 
-    if (steering.magnitude() > maxForce){  // mais ils ont deja la meme vitesse donc 0 diff a part si on ajoute des tailles differentes 
+    steering -= velocity;     
+    if (steering.magnitude() > maxForce){ 
       steering =steering.normalized() * maxForce; 
     }
   }
-  return steering;   //retourne le vecteur (force d'alignement)
+  return steering;  
 }
 
 
@@ -141,7 +141,7 @@ simulation::Vector2D simulation::Boid::cohesion(const std::vector<Boid*>& boids)
   }
   center= center /static_cast<float>(count);
 
-  return seek(center);      //utiliser seek pour aller vers le centre
+  return seek(center);     
 }
 
 
@@ -176,4 +176,3 @@ simulation::Vector2D simulation::Boid::flee(const Vector2D& target) {
   }
   return steering; 
 }
-//rajouter warp around et rebondir
