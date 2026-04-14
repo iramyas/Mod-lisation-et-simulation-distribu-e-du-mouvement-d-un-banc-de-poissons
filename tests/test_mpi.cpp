0b4d222fef8d5a63acae839cde50a3cb25c8e01a
Yohann Front-Reignier <yohann.front-reignier@ens.uvsq.fr>
2026-04-14 15:01:59 +0200

    fix: missing file + fix: test MPI failing and deadlocking

--- tests/test_mpi.cpp
+++ tests/test_mpi.cpp
@@ -12,6 +12,9 @@ protected:
     static void SetUpTestSuite() {
         // MPI déjà initialisé par le main de GTest
     }
+    static void TearDownTestSuite() {
+        
+    }
 };
 
 TEST_F(MPITest, DomainDecomposition) {
@@ -38,6 +41,8 @@ TEST_F(MPITest, HaloExchange) {
 }
 
 TEST_F(MPITest, SyncGlobalCount) {
+    MPI_Barrier(MPI_COMM_WORLD);
+    
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     
@@ -48,4 +53,17 @@ TEST_F(MPITest, SyncGlobalCount) {
     int globalCount = flock.getGlobalBoidCount();
     EXPECT_GT(globalCount, 0);
     EXPECT_GE(globalCount, (int)flock.boids.size());
+
+    MPI_Barrier(MPI_COMM_WORLD);
+}
+
+// Main qui gère MPI_Init/Finalize
+int main(int argc, char** argv) {
+    MPI_Init(&argc, &argv);
+    
+    ::testing::InitGoogleTest(&argc, argv);
+    int result = RUN_ALL_TESTS();
+    
+    MPI_Finalize();
+    return result;
 }
